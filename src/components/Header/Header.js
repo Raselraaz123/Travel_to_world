56cd8b4d1db03cde59621a4c9428be463dda8b22
Raselraaz123 <raselraaz91@gmail.com>
2022-04-17 19:51:13 +0600

    Footer ok

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,19 +5,19 @@ import logo from '../../images/apple-touch-icon.png'
 
 const Header = () => {
   return (
-    <nav className="nav-container">
-      <div className="logo-style">
-        <Link to='/'>
-
-          <img src={logo} alt="" />
-        </Link>
-      </div>
-      <div className="nav-link-style font-style">
-        <Link to="/">Home</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/about">About me</Link>
-        <Link to="/login">Login</Link>
-      </div>
+    <nav className=" nav-container">
+        <div className="logo-style">
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="nav-link-style ">
+          <Link to="/">Home</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/about">About me</Link>
+          <Link to="/login">Login</Link>
+        </div>
     </nav>
   );
 };
--- src/components/Page/Footer/Footer.js
+++ src/components/Page/Footer/Footer.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import logo from '../../../images/apple-touch-icon.png'
+import facebook from '../../../images/social-icon/facebook.png'
+import google from '../../../images/social-icon/google.png'
+
+
 import './Footer.css'
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
   return (
-    <footer className='footer-container'>
+    <footer className="footer-container">
       <div>
         <img src={logo} alt="" />
       </div>
       <div>
-        <p>Copyright © {year} Company Name</p>
+        <p>Copyright &reg; {year} All rights reserved</p>
       </div>
       <div>
-        <button>Email & password</button>
-        <button>Login in google</button>
-        <button> Facebook</button>
+        <button className="w-100 px-5 mb-2 shadow-lg border-0">
+          <img width={40} src={google} alt="" /> google
+        </button>
+        <br />
+        <button className="w-100 shadow-lg border-0">
+          <img src={facebook} alt="" /> Facebook
+        </button>
       </div>
     </footer>
   );
