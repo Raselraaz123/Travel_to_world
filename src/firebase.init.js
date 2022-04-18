66651d33ef14b2ecf29ca8b191b2a548c11e8728
Raselraaz123 <raselraaz91@gmail.com>
2022-04-18 15:57:07 +0600

    env.local ok

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHoSf-2Km_wjBvT9gdo9qJdZf1q6l1cEg",
-  authDomain: "travel-to-world-7da9f.firebaseapp.com",
-  projectId: "travel-to-world-7da9f",
-  storageBucket: "travel-to-world-7da9f.appspot.com",
-  messagingSenderId: "808464019556",
-  appId: "1:808464019556:web:2ddc85fa6bccfdb514ef71",
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 
